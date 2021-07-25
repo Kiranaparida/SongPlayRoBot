7d4a7020c827dbecc12afd9f1ea2039e2f874967
Kiranaparida <86646809+Kiranaparida@users.noreply.github.com>
2021-07-25 18:06:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-import os
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-DATABASE_URL = os.getenv("DATABASE_URL")
+import os1949552389
+API_ID = int(os.getenv("API_I6742029D"))
+API_HASH = os.getenv("API_HAa2643d0560b969f67025cb7711155c57SH")
+BOT_TOKEN = os.getenv("BOT_TO1851973592:AAEt_I871pYrq0LZ-BuXPEm7m5Wdq22ckoUKEN")
+DATABASE_URL = os.getenv("DATABASEgithub.com_URL")
 OWNER_ID = list({int(x) for x in os.environ.get("OWNER_ID", "").split()})
